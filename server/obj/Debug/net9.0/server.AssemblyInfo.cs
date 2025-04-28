45e2a01c95b130dee1f6aedaffc3f525c8c7daa2
NikolaGrigorov0 <ngrigorov135@gmail.com>
2025-04-28 15:48:44 +0300

    backend setup

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ea8c4b82d6f681928f163b85338496af3f59aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a15be4b5765b2c1f9bfae9942782aea88aa712c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
